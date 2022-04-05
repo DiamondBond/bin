dd2c33f9bba6da88997520450b4f4fcf44f1dd6f
Diamond Bond <diamond.nigel75@Gmail.com>
2022-04-05 16:50:58 +0530

    update

--- updatespotify.sh
+++ updatespotify.sh
@@ -5,3 +5,6 @@ cd ~/git/spotify-adblock
 git pull
 make
 sudo make install
+mkdir -p ~/.spotify-adblock && cp target/release/libspotifyadblock.so ~/.spotify-adblock/spotify-adblock.so
+mkdir -p ~/.config/spotify-adblock && cp config.toml ~/.config/spotify-adblock
+flatpak override --user --filesystem="~/.spotify-adblock/spotify-adblock.so" --filesystem="~/.config/spotify-adblock/config.toml" com.spotify.Client
