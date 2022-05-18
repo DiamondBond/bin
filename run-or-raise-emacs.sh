099bc51d6157d49c129cbd8062bc2ba9394c77af
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-18 05:30:14 +0530

    update scripts

---
+++ run-or-raise-emacs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# get id of emacs
+win=$(xdotool search --onlyvisible --class emacs | head -1)
+
+launch() {
+    echo setsid nohup "$*" &
+    setsid nohup "$*" &
+}
+
+launchcmd="emacsclient -c -n -a 'emacs'"
+
+
+if [ "x$win" = x ]; then
+    # start Emacs
+    #exec /usr/local/bin/emacsclient -c -n -a 'emacs' &
+    launch $launchcmd
+else
+    # Emacs is already running...
+    if [ $win = $(xdotool getwindowfocus) ]; then
+        # ...and focused
+        # do nothing
+        sleep 0.2;
+    else
+        # switch to it
+        xdotool windowactivate $win
+    fi
+fi
