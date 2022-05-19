a323fbced6931efeaed3bcf96f554bb9b6bfa691
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-20 04:32:12 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -3,28 +3,28 @@
 interval=0.2
 
 # bin
+echo "\n========== SYNCING: BIN =========="
 cd ~/bin
 git add -A
 git commit -asm "Automatic sync"
 git push
-
-echo "========== Synced bin =========="
+echo "========== DONE: BIN ==========\n"
 
 # dotfiles
+echo "\n========== SYNCING: DOTS =========="
 cp -r ~/.config/openbox ~/git/dotfiles/.config/
 cd ~/git/dotfiles
 ./backup.sh
 git add -A
 git commit -asm "Automatic sync"
 git push
-
-echo "========== Synced dotfiles =========="
+echo "========== DONE: DOTS ==========\n"
 
 # emacs
+echo "\n========== SYNCING: EMACS =========="
 cp ~/.emacs.d/config.org ~/git/emacs/
 cd ~/git/emacs
 git add -A
 git commit -asm "Automatic sync"
 git push
-
-echo "========== Synced emacs =========="
+echo "========== DONE: EMACS ==========\n"
