6497f62edf18f6afe618a1d4f6907dc46d2a28ae
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-19 21:39:31 +0530

    update ~/bin

---
+++ switchblade_openbox.sh
@@ -0,0 +1,2 @@
+pkill xfconf
+cp ~/bin/disabled-xfwm.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
---
+++ switchblade_xfce.sh
@@ -0,0 +1,2 @@
+pkill xfconf
+cp ~/bin/enabled-xfwm.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
