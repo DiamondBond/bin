e246c173b98cd9ff9f30befae86ecbbceb44cdce
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-20 21:21:24 +0530

    update btmpanel script

--- bottompanel.sh
+++ bottompanel.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Wait for all panels to load
-while [ "$(wmctrl -l | grep -c xfce4-panel)" -lt "2" ];
-do
-   sleep 0.05s
-done
+#while [ "$(wmctrl -l | grep -c xfce4-panel)" -lt "2" ];
+#do
+#   sleep 0.05s
+#done
 
 # Determine identifier
-ID=$(wmctrl -l | grep xfce4-panel | sed 3p | awk '{ print $1 }' | tail -1)
+#ID=$(wmctrl -l | grep xfce4-panel | sed 3p | awk '{ print $1 }' | tail -1)
 # Place panel at desktop-level
-wmctrl -i -r $ID -b add,below
+#wmctrl -i -r $ID -b add,below
+
+wmctrl -a :SELECT: -b add,below
