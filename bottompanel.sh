ef684a8f3b07fb7df26758e027acd447a7f5a51f
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-21 00:02:36 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- bottompanel.sh
+++ bottompanel.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Wait for all panels to load
-while [ "$(wmctrl -l | grep -c xfce4-panel)" -lt "2" ];
-do
-   sleep 0.05s
-done
+#while [ "$(wmctrl -l | grep -c xfce4-panel)" -lt "2" ];
+#do
+#   sleep 0.05s
+#done
+
+# Just wait 5s for panel to load
+sleep 5;
 
 # Determine identifier
 ID=$(wmctrl -l | grep xfce4-panel | sed 3p | awk '{ print $1 }' | tail -1)
-notify-send `$ID`
+
 # Place panel at desktop-level
 wmctrl -i -r $ID -b add,below
-notify-send "Panel should be below"
-# manual
-#wmctrl -a :SELECT: -b add,below
