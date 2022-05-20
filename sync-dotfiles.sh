220a8c765cfd49a41cd05b33feea3e3d1be88e13
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-20 21:55:56 +0530

    sync-dots: now backs up xfce & ob theme
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -17,6 +17,11 @@ echo
 echo "========== SYNC: DOTS =========="
 cp -r ~/.config/openbox ~/git/dotfiles/.config/
 cp ~/.config/gtk-3.0/gtk.css ~/git/dotfiles/.config/gtk-3.0/
+cp -r ~/.themes/win ~/git/dotfiles/.themes/
+cp -r ~/.config/xfce4/helpers.rc ~/git/dotfiles/.config/xfce4/
+cp -r ~/.config/xfce4/panel ~/git/dotfiles/.config/xfce4/
+cp -r ~/.config/xfce4/terminal ~/git/dotfiles/.config/xfce4/
+cp -r ~/.config/xfce4/xfconf ~/git/dotfiles/.config/xfce4/
 cd ~/git/dotfiles
 ./backup.sh
 git add -A
