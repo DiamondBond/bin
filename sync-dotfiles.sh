d1b48b98d3b2414f8120aa7f891dd76b386eedd5
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-23 10:04:53 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -15,9 +15,14 @@ echo
 # dotfiles
 echo
 echo "========== SYNC: DOTS =========="
+# Openbox
 cp -r ~/.config/openbox ~/git/dotfiles/.config/
-cp ~/.config/gtk-3.0/gtk.css ~/git/dotfiles/.config/gtk-3.0/
+# Themes
 cp -r ~/.themes/win ~/git/dotfiles/.themes/
+cp -r ~/.themes/dolphin ~/git/dotfiles/.themes/
+# GTK CSS
+cp ~/.config/gtk-3.0/gtk.css ~/git/dotfiles/.config/gtk-3.0/
+# XFCE4
 cp -r ~/.config/xfce4/helpers.rc ~/git/dotfiles/.config/xfce4/
 cp -r ~/.config/xfce4/panel ~/git/dotfiles/.config/xfce4/
 cp -r ~/.config/xfce4/terminal ~/git/dotfiles/.config/xfce4/
