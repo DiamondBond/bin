e7ea0d52bd2f452d8c39ddf7f31a95f626fd3b50
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-23 11:52:00 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -24,7 +24,7 @@ cp -r ~/.themes/dolphin ~/git/dotfiles/.themes/
 cp ~/.config/gtk-3.0/gtk.css ~/git/dotfiles/.config/gtk-3.0/
 # XFCE4
 cp -r ~/.config/xfce4/helpers.rc ~/git/dotfiles/.config/xfce4/
-cp -r ~/.config/xfce4/panel ~/git/dotfiles/.config/xfce4/
+#cp -r ~/.config/xfce4/panel ~/git/dotfiles/.config/xfce4/
 cp -r ~/.config/xfce4/terminal ~/git/dotfiles/.config/xfce4/
 cp -r ~/.config/xfce4/xfconf ~/git/dotfiles/.config/xfce4/
 cd ~/git/dotfiles
