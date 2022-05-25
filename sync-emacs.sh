667e5463474970b6f43e5509d92b3912ad842831
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-26 03:35:16 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- sync-emacs.sh
+++ sync-emacs.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 cp ~/.emacs.d/early-init.el ~/git/emacs/
 cp ~/.emacs.d/init.el ~/git/emacs/
 cp ~/.emacs.d/config.org ~/git/emacs/
-cp ~/.emacs.d/globals.org ~/git/emacs/
+#cp ~/.emacs.d/globals.org ~/git/emacs/
 
 # user
-cp ~/.emacs.d/userconfig.org ~/git/emacs/
+#cp ~/.emacs.d/userconfig.org ~/git/emacs/
 cp -r ~/.emacs.d/modules ~/git/emacs/
 #cp -r ~/.emacs.d/snippets ~/git/emacs/
 cp -r ~/.emacs.d/resources ~/git/emacs/
