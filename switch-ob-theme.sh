c5a9eb69069f300bfc277b26c70c43e56000d32a
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-25 17:28:27 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- switch-ob-theme.sh
+++ switch-ob-theme.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
 # switch openbox theme
 clear
 echo -e "Choose openbox theme \n"
 PS3="Type a number or 'q' to quit: "
 fileList=$(find ~/.themes -type d -name "openbox-3"  | cut -d "/" -f5)
+ob_rc="$HOME/.config/openbox/rc.xml"
 
 select fileName in $fileList; do
     if [ -n "$fileName" ]; then
-    sed -i "s/$(grep "<theme>" ~/.config/openbox/rc.xml -A 5 | grep "<name>"| awk -F"[><]" '{print $3}')/$fileName/g" ~/.config/openbox/rc.xml
+    # priceless piece of information: https://superuser.com/a/508143
+    xmlstarlet ed -L -N o="http://openbox.org/3.4/rc" -u '/o:openbox_config/o:theme/o:name' -v "$fileName" "$ob_rc"
     openbox --reconfigure
     fi
     break
