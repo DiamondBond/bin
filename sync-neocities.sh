3cee8bbd8dc7f664d0c0e0089cf17b5b308764ff
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-25 19:43:06 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- sync-neocities.sh
+++ sync-neocities.sh
@@ -9,16 +9,20 @@ then
 	echo
 
 	# purge old html files
-	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete index.html
+	echo "Purging old files on neocities.org"
+	#echo "Skipping deleting index.html as we cannot delete index.html"
+	#/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete index.html
 	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete resume.html
 	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete emacs.html
 
 	# generate new html files
+	echo "Compiling new files locally"
 	emacsclient -e "(progn (find-file \"~/Projects/neocities/index.org\") (org-html-export-to-html))"
 	emacsclient -e "(progn (find-file \"~/git/resume/resume.org\") (org-html-export-to-html))"
 	emacsclient -e "(progn (find-file \"~/.emacs.d/config.org\") (org-html-export-to-html))"
 
 	# upload new html files
+	echo "Uploading new files to neocities.org"
 	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/Projects/neocities/index.html
 	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/git/resume/resume.html
 	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/.emacs.d/emacs.html
@@ -28,6 +32,6 @@ then
 
 else
 	echo
-    echo "ERROR: emacs is not running, abandoning sync."
+	echo "ERROR: emacs is not running, abandoning sync."
 	echo
 fi
