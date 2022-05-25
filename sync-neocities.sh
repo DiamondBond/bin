c41412c1a83b2661f3527912d5369fadfac03da3
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-25 19:35:06 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

---
+++ sync-neocities.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+
+if pgrep 'emacs' > /dev/null
+then
+	# sync website to neocities
+	echo
+	echo "========== SYNC: NEOCITIES =========="
+	echo
+
+	# purge old html files
+	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete index.html
+	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete resume.html
+	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete emacs.html
+
+	# generate new html files
+	emacsclient -e "(progn (find-file \"~/Projects/neocities/index.org\") (org-html-export-to-html))"
+	emacsclient -e "(progn (find-file \"~/git/resume/resume.org\") (org-html-export-to-html))"
+	emacsclient -e "(progn (find-file \"~/.emacs.d/config.org\") (org-html-export-to-html))"
+
+	# upload new html files
+	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/Projects/neocities/index.html
+	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/git/resume/resume.html
+	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/.emacs.d/emacs.html
+	echo
+	echo "========== DONE: NEOCITIES =========="
+	echo
+
+else
+	echo
+    echo "ERROR: emacs is not running, abandoning sync."
+	echo
+fi
