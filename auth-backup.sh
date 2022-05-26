ba448ab076824e25dd5d3d5e0764732c8f5dcfb3
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-26 07:05:04 +0530

    Automatic sync
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- auth-backup.sh
+++ auth-backup.sh
@@ -5,4 +5,4 @@ cp ~/.authinfo.gpg ~/org/
 cp ~/.emacs.d/secrets.el.gpg ~/org/
 cp ~/.emacs.d/network-security.data ~/org/
 cp ~/.emacs.d/custom.el ~/org/
-cp ~/.emacs.d/userconfig.org ~/org/
+#cp ~/.emacs.d/userconfig.org ~/org/
--- auth-restore.sh
+++ auth-restore.sh
@@ -5,4 +5,4 @@ cp ~/org/.authinfo.gpg ~/
 cp ~/org/secrets.el.gpg ~/.emacs.d/
 cp ~/org/network-security.data ~/.emacs.d/
 cp ~/org/custom.el ~/.emacs.d/
-cp ~/org/userconfig.org ~/.emacs.d/
+#cp ~/org/userconfig.org ~/.emacs.d/
