d16c782292b327c1e899d44ee48afb0ed0f6664a
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-27 06:11:54 +0530

    update auth scripts with comments

--- auth-backup.sh
+++ auth-backup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# secrets
 cp ~/.authinfo.gpg ~/org/
 cp ~/.secrets.el.gpg ~/org/
 cp ~/.emacs.d/network-security.data ~/org/
+
+# user related
 cp ~/.emacs.d/custom.el ~/org/
 cp ~/.emacs.d/userconfig.org ~/org/
--- auth-restore.sh
+++ auth-restore.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# Emacs looks for these files in ~/
 cp ~/org/.authinfo.gpg ~/
 cp ~/org/.secrets.el.gpg ~/
+
+# Emacs looks for these files in ~/.emacs.d
 cp ~/org/network-security.data ~/.emacs.d/
 cp ~/org/custom.el ~/.emacs.d/
 cp ~/org/userconfig.org ~/.emacs.d/
