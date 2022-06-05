99adab6170f579f6f086357ac731f5266a15f684
Diamond Bond <diamond.nigel75@Gmail.com>
2022-06-06 04:58:40 +0530

    Update

--- sync-neocities.sh
+++ sync-neocities.sh
@@ -10,10 +10,10 @@ then
 
 	# purge old html files
 	echo "Purging old files on neocities.org"
-	#echo "Skipping deleting index.html as we cannot delete index.html"
+	#echo "Skip deleting index.html as we cannot delete it"
 	#/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete index.html
-	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete resume.html
-	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities delete emacs.html
+	neocities delete resume.html
+	neocities delete emacs.html
 
 	# generate new html files
 	echo "Compiling new files locally"
@@ -23,9 +23,9 @@ then
 
 	# upload new html files
 	echo "Uploading new files to neocities.org"
-	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/Projects/neocities/index.html
-	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/git/resume/resume.html
-	/home/diamond/.local/share/gem/ruby/3.0.0/bin/neocities upload ~/.emacs.d/emacs.html
+	neocities upload ~/Projects/neocities/index.html
+	neocities upload ~/git/resume/resume.html
+	neocities upload ~/.emacs.d/emacs.html
 	echo
 	echo "========== DONE: NEOCITIES =========="
 	echo
