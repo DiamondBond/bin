effb64debec8dd6646e297903abba0d90c4388fe
Diamond Bond <diamondbond1@gmail.com>
2023-01-03 02:21:35 +0530

    update hide titlebar script

--- hidetitlebar.sh
+++ hidetitlebar.sh
@@ -4,14 +4,11 @@ TOGGLE=/tmp/toggle_titlebar
 if [ ! -e $TOGGLE ]; then
     touch $TOGGLE
     xprop \
-	-id $(xdotool getactivewindow) \
 	-f _MOTIF_WM_HINTS 32c \
-	-set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0"
+	-set _MOTIF_WM_HINTS 2
 else
     rm $TOGGLE
     xprop \
-	-id $(xdotool getactivewindow) \
 	-f _MOTIF_WM_HINTS 32c \
-	-set _MOTIF_WM_HINTS "0x2, 0x0, 0x1, 0x0, 0x0"
+	-set _MOTIF_WM_HINTS 1
 fi
-
